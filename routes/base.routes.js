64c13a2636db4ae273a742dfffe15afc8d8cc2a6
Avishka Indula <avishkainduladev@gmail.com>
2022-11-25 07:42:56 +0530

    Adding Logout Functionality
    => Wrapping Logout button in a form and send a post request in header.ejs file.
    => Creating destroyUserAuthSession function inside authentication.js file.
    => Creating a logout function in auth.controller.js and executing destroyUserAuthSession() function there.
    => Registering a post logout route in auth.routes.js file and pass the logout function to it.
    => Fixing a issue of a comment I wrote in base.routes.js file.
    => I thought only logged in users can access /products page.
    => But turns out, everyone should be a

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.get("/", function (req, res) {
   res.redirect("/products");
 });
-// Now when a logged in user try to visit the starting page, he will be redirects to /products page.
+// Now when a user try to visit the starting page, he will be redirected to /products page.
+// Whether the user is logged in or not, it doesn't matter. All users will be redirected to /products
 
 module.exports = router;
