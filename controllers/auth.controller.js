5bdba1ace0a244d32378dea7ef32459a681cbe00
Avishka Indula <avishkainduladev@gmail.com>
2022-11-25 08:17:52 +0530

    Handling errors in async code
    => Unfortunately the default express 500 error handling middleware we created on
    => error-handler.js ignores errors that occur inside async operations.
    => Therefor we need to use try catch to create our own error handling for those.
    => Wrapping await user.signup() in a try catch block inside auth.controller.js file
    => Adding a next parameter to signup function
    => writing next(error) inside catch block
    => Now the default error handling middleware will become active.
    => Basically we need to wrap all that has a "await" keyword in it inside a try catch block.
    => Wrapping existingUser of login function inside auth.controller.js with a try catch block.
    => Make existingUser available from the outside of the function by using let existingUser

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,7 +5,7 @@ function getSignup(req, res) {
   res.render("customer/auth/signup");
 }
 
-async function signup(req, res) {
+async function signup(req, res, next) {
   const user = new User(
     req.body.email,
     req.body.password,
@@ -15,8 +15,13 @@ async function signup(req, res) {
     req.body.city
   );
 
-  await user.signup();
-  // This will trigger the signup method inside User class
+  try {
+    await user.signup();
+    // This will trigger the signup method inside User class
+  } catch (error) {
+    next(error);
+    return;
+  }
 
   res.redirect("/login");
 }
@@ -25,12 +30,19 @@ function getLogin(req, res) {
   res.render("customer/auth/login");
 }
 
-async function login(req, res) {
+async function login(req, res, next) {
   const user = new User(req.body.email, req.body.password);
   // This will assign email and password by extracting them through the request
   // and assign them to email and password parameters of User class and omit fullname, street,
   // postal and city parameters.
-  const existingUser = await user.getUserWithSameEmail();
+  let existingUser;
+  try {
+    // const existingUser = await user.getUserWithSameEmail();
+    existingUser = await user.getUserWithSameEmail();
+  } catch (error) {
+    next(error);
+    return;
+  }
 
   if (!existingUser) {
     res.redirect("/login");
