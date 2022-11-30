605cb65786971f03fa6ea44e043d618a34a3cf29
Avishka Indula <avishkainduladev@gmail.com>
2022-11-30 09:28:20 +0530

    Adding product admin pages and forms
    => creating admin.routes.js file.
    => importing admin.routes.js files into app.js file.
    => creating admin.controller.js file.
    => linking admin.controller.js file into admin.routes.js file.
    => creating all-products.ejs file inside admin views folder.
    => creating new-product.ejs file inside admin views folder.
    => linking all-products.ejs file to getProducts function in admin.controller.js file.
    => linking new-product.ejs file to getNewProduct function in admin.controller.js file.
    => styling reset button of new-product.ejs file through base.css file.

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const path = require("path");
-
 const express = require("express");
-
 const csurf = require("csurf");
 const expressSession = require("express-session");
 
 const createSessionConfig = require("./config/session");
 const db = require("./data/database");
 
-const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
-
+const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
-
 const checkAuthStatusMiddleware = require("./middlewares/check-auth");
 
 const authRoutes = require("./routes/auth.routes");
-
 const productsRoutes = require("./routes/products.routes");
 const baseRoutes = require("./routes/base.routes");
+const adminRoutes = require("./routes/admin.routes");
 
 const app = express();
 
@@ -40,6 +36,11 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
+app.use("/admin", adminRoutes);
+// This will act as a filter
+// Only paths that start with /admin will make it into the admin.routes rote configuration.
+// So we don't wanna add /admin in the routes we create on admin.routes.js file.
+// But we still need to add /admin in the links we create on ejs files.
 
 app.use(errorHandlerMiddleware);
 
