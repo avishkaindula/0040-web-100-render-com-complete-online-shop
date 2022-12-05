a0e794e913affc5b6f21424ccb898e22366fc043
Avishka Indula <avishkainduladev@gmail.com>
2022-12-05 19:00:56 +0530

    Working On The Shopping Cart Logic
    => overwrite the cart in the session with cart data from res.locals.cart
    *  inside addCartItem() function of cart.controller.js file.
    => sending a JS driven request to the backend by using res.status(201).json(); in that function.
    => adding this.totalQuantity and this.totalPrice to cart.model.js file.
    => adding this.totalQuantity++; and this.totalPrice += product.price; to the Product class.
    => sending cart.totalQuantity along with the res.status(201).json() data in cart.controller.js file.
    => creating .badge {} class in base.css file.
    => adding a span with the .badge {} class to the Cart tab in nav-items.ejs file and inject
    *  <%= locals.cart.totalQuantity %> to it.
    => fixed a minor problem in cart.js which became a major headache for me.

--- middlewares/cart.js
+++ middlewares/cart.js
@@ -9,11 +9,11 @@ function initializeCart(req, res, next) {
     cart = new Cart(req.session.cart.items);
     // This else block will become active if the user already has a cart items list.
     // req.session.cart.items will take the items from the cart that was already stored in the session.
-
-    res.locals.cart = cart;
-
-    next();
   }
+
+  res.locals.cart = cart;
+
+  next();
 }
 
 module.exports = initializeCart;
