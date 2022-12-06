6b6d542d764d607d05124b94c1e0e24bce58c04a
Avishka Indula <avishkainduladev@gmail.com>
2022-12-06 21:16:14 +0530

    Adding A Cart Page
    => creating the cart.ejs file.
    => creating cart-item.ejs include.
    => creating the getCart function in cart.controller.js file.
    => creating router.get("/"); function in cart.routes.js file.
    => fixing a minor issue in cart.model.js file.
    => creating the cart.css file.

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -4,6 +4,9 @@ const cartController = require("../controllers/cart.controller");
 
 const router = express.Router();
 
+router.get("/", cartController.getCart);
+// This is actually /cart/ route.
+
 router.post("/items", cartController.addCartItem);
 
 module.exports = router;
